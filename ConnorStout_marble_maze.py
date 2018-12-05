409ce3c4786dbbe7bcdf38e30d5dee4b3722e9a5
cstout994 <45598588+cstout994@users.noreply.github.com>
2018-12-05 15:05:11 +0000

    Add files via upload

--- ConnorStout_marble_maze.py
+++ ConnorStout_marble_maze.py
@@ -16,18 +16,49 @@ maze = [[r,r,r,r,r,r,r,r],
         [r,b,b,r,b,b,b,r],
         [r,r,r,r,r,r,r,r]]
 
-sense.set_pixels(sum(maze,[]))
+game_over = False
 
+def move_marble(pitch, roll, x, y):
+    new_x = x
+    new_y = y
+    if 1 < pitch < 179:
+        new_x -= 1
+    elif 359 > pitch > 181 and x != 7:
+        new_x +=1
+    elif 359 > roll > 179 and y != 0:
+        new_y -= 1
+    return new_x, new_y
+
+def check_wall(x,y,new_x,new_y):
+    if maze[new_y][new_x] != r:
+        return new_x, new_y
+    elif maze [new_y][x] != r:
+        return x, new_y
+    elif maze[y][new_x] != r:
+        return new_x, y
+    else:
+        return x,y
+
+while game_over == False:
+    o = sense.get_orientation()
+    pitch = o["pitch"]
+    roll = o["roll"]
+    x,y = move_marble(pitch,roll,x,y)     
+    maze[y][x] = w
+    sense.set_pixels(sum(maze,[]))
+while not game_over:
+    pitch = sense.get_orientation()['pitch']
+    roll = sense.get_orientation()['roll']
+    x,y = move_marble(pitch,roll,x,y)
+    maze[y][x] = w
+    sense.get_pixels(sum(maze,[]))
+    sleep(0.05)
+    maze[y][x] = b
 from sense_hat import SenseHat
 
 sense = SenseHat()
-
-r = 255
-g = 255
-b = 255
-
-sense.clear((r, g, b))
-
+sense.clear()
+              
 r = (255,0,0)
-
+b = (0,0,0)
 w = (255,255,255)
