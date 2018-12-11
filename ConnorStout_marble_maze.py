86e576d8d764cfa7bcd348d325f000a7b7d28ff4
cstout994 <45598588+cstout994@users.noreply.github.com>
2018-12-11 08:07:48 -0600

    Add files via upload

--- ConnorStout_marble_maze.py
+++ ConnorStout_marble_maze.py
@@ -4,9 +4,8 @@ sense = SenseHat()
 sense.clear()
 
 r = (255,0,0)
+
 b = (0,0,0)
-w = (255,255,255)
-g = (0,255,0)
 
 maze = [[r,r,r,r,r,r,r,r],
         [r,b,b,b,b,b,b,r],
@@ -14,49 +13,21 @@ maze = [[r,r,r,r,r,r,r,r],
         [r,b,r,b,r,r,r,r],
         [r,b,b,b,b,b,b,r],
         [r,b,r,r,r,r,b,r],
-        [r,b,b,r,b,b,g,r],
+        [r,b,b,r,b,b,b,r],
         [r,r,r,r,r,r,r,r]]
 
-game_over = False
+sense.set_pixels(sum(maze,[]))
 
-def move_marble(pitch, roll, x, y):
-    new_x = x
-    new_y = y
-    if 1 < pitch < 179:
-        new_x -= 1
-    elif 359 > pitch > 181 and x != 7:
-        new_x +=1
-    elif 359 > roll > 179 and y != 0:
-        new_y -= 1
-    return new_x, new_y
+from sense_hat import SenseHat
 
-def check_wall(x,y,new_x,new_y):
-    if maze[new_y][new_x] != r:
-        return new_x, new_y
-    elif maze [new_y][x] != r:
-        return x, new_y
-    elif maze[y][new_x] != r:
-        return new_x, y
-    else:
-        return x,y
+sense = SenseHat()
 
-while game_over == False:
-    o = sense.get_orientation()
-    pitch = o["pitch"]
-    roll = o["roll"]
-    x,y = move_marble(pitch,roll,x,y)     
-    if maze[y][x] == g:   # My winning marker was green
-      sense.show_message("Win!")
-      game_over = True
-    maze[y][x] = w
-    sense.set_pixels(sum(maze,[]))
-    sleep(0.05)
-    maze[y][x] = b
-while not game_over:
-    pitch = sense.get_orientation()['pitch']
-    roll = sense.get_orientation()['roll']
-    x,y = move_marble(pitch,roll,x,y)
-    maze[y][x] = w
-    sense.get_pixels(sum(maze,[]))
-    sleep(0.05)
-    maze[y][x] = b
+r = 255
+g = 255
+b = 255
+
+sense.clear((r, g, b))
+
+r = (255,0,0)
+
+w = (255,255,255)
