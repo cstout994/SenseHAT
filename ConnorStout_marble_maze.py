418f48ea097f24d93f79a7148adfa52b2235ed63
cstout994 <45598588+cstout994@users.noreply.github.com>
2018-12-14 14:05:24 +0000

    Add files via upload

--- ConnorStout_marble_maze.py
+++ ConnorStout_marble_maze.py
@@ -26,18 +26,7 @@ sense.set_pixels(sum(maze,[]))
 maze[y][x] = w
 
 game_over = False
-def move_marble(pitch, roll, x, y):
-   new_x = x
-   new_y = y
-   if 1 < pitch < 179:
-        new_x -= 1
-   elif 359 > pitch > 181 and x != 7:
-        new_x +=1
-   elif 359 > roll > 179 and y != 0:
-        new_y -= 1
-   if 359 < pitch < 181:
-       new_x +=1
-   return new_x, new_y
+
 def check_wall(x,y,new_x,new_y):
    if maze[new_y][new_x] != r:
         return new_x, new_y
@@ -47,16 +36,31 @@ def check_wall(x,y,new_x,new_y):
         return new_x, y
    else:
         return x,y
+
+def move_marble(pitch, roll, x, y):
+   new_x = x
+   new_y = y
+   check_wall(x,y,new_x,new_y)
+   new_x, new_y = check_wall(x,y, new_x, new_y)
+   if 1 < pitch < 179:
+        new_x -= 1
+   elif 359 > pitch > 181 and x != 7:
+        new_x +=1
+   elif 359 > roll > 179 and y != 0:
+        new_y -= 1
+   if 359 < pitch < 181:
+       new_x +=1
+   return new_x, new_y
+
 while game_over == False:
     o = sense.get_orientation()
     pitch = o["pitch"]
     roll = o["roll"]
     yaw = o["yaw"]
     x,y = move_marble(pitch,roll,x,y)     
-    maze[y][x] = w
-    if maze[y][x] == g:   
-      sense.show_message("Win!")
-      game_over = True
+    if maze[y][x] == g: 
+        sense.show_message("Win!")
+        game_over = True
     maze[y][x] = w
     sense.set_pixels(sum(maze,[]))
     sleep(0.05)
@@ -75,5 +79,3 @@ r = 255
 g = 255
 b = 255
 sense.clear((r, g, b))
-r = (255,0,0)
-w = (255,255,255)
